04c3713e7396f7738fda9efd9ab06f253ae837b5
kulchynski <@Viachaslau95>
2023-05-05 11:26:39 +0300

    added first view

--- service/services/models.py
+++ service/services/models.py
@@ -8,6 +8,9 @@ class Service(models.Model):
     name = models.CharField(max_length=100)
     full_price = models.PositiveIntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class Plan(models.Model):
     PLAN_TYPES = (
@@ -21,8 +24,12 @@ class Plan(models.Model):
                                                        MaxValueValidator(100)
                                                    ])
 
+    def __str__(self):
+        return self.plan_types
+
 
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+        
