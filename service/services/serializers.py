5f2f5550492313e64dbdfc772e0fdfbb30ef54d1
kulchynski <@Viachaslau95>
2023-05-10 12:41:28 +0300

    added new field in API "total_amount"

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -26,7 +26,5 @@ class SubscriptionSerializers(serializers.ModelSerializer):
         )
 
     def get_price(self, instance):
-        return instance.service.full_price - instance.service.full_price * (
-                instance.plan.discount_percent / 100
-        )
+        return instance.price
 
