3797ebd03a5ea3c9b7a017d3e51e65f64cbbf09e
kulchynski <@Viachaslau95>
2023-05-18 14:55:11 +0300

    optimize cache connected RedisCache

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -3,6 +3,8 @@ import time
 
 from celery import shared_task
 from celery_singleton import Singleton
+from django.conf import settings
+from django.core.cache import cache
 from django.db import transaction
 from django.db.models import F
 
@@ -25,7 +27,7 @@ def set_price(subscription_id):
 
         subscription.price = subscription.annotated_price
         subscription.save()
-
+    cache.delete(settings.PRICE_CACHE_NAME)
 
 @shared_task(base=Singleton)
 def set_comment(subscription_id):
@@ -35,5 +37,6 @@ def set_comment(subscription_id):
         subscription = Subscription.objects.select_for_update().get(id=subscription_id)
         subscription.comment = str(datetime.datetime.now())
         subscription.save()
+    cache.delete(settings.PRICE_CACHE_NAME)
 
 
